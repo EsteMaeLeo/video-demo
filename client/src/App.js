bf19ac72d903335d032cff7b7d549080f34d7b1e
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-25 22:25:21 -0500

    initial context

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,46 @@
 import React from "react";
 import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 import VideoPlayer from "./components/VideoPlayer";
 import Options from "./components/Options";
 import Notifications from "./components/Notifications";
 
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    borderRadius: 35,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "4px solid #c8e6c9",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
+
 const App = () => {
+  const classStyles = useStyles();
   return (
-    <div>
-      <AppBar position="static" style={{ background: "#a5d6a7" }}>
+    <div className={classStyles.wrapper}>
+      <AppBar
+        className={classStyles.appBar}
+        position="static"
+        style={{ background: "#a5d6a7" }}
+      >
         <Typography variant="h2" align="center">
           Video DEVS
         </Typography>
---
+++ client/src/Context.js
@@ -0,0 +1,31 @@
+import React, { createContext, useState, useRef, useEffect } from "react";
+import { io } from "socket.io-client";
+import Peer from "simple-peer";
+
+const SocketContext = createContext();
+
+const socket = io("http://localhost:5000");
+
+const ContextProvider = ({ children }) => {
+  useEffect(() => {});
+
+  const answerCall = () => {};
+
+  const callUser = id => {};
+
+  const leaveCall = () => {};
+
+  return (
+    <SocketContext.Provider
+      value={{
+        callUser,
+        leaveCall,
+        answerCall,
+      }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export { ContextProvider, SocketContext };
