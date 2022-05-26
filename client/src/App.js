76549978c8699c404e35170b494107fa79893fab
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-25 22:11:13 -0500

    create files adding new app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
 
-function App() {
-  return <div>App</div>;
-}
+import VideoPlayer from "./components/VideoPlayer";
+import Options from "./components/Options";
+import Notifications from "./components/Notifications";
+
+const App = () => {
+  return (
+    <div>
+      <AppBar position="static" style={{ background: "#a5d6a7" }}>
+        <Typography variant="h2" align="center">
+          Video DEVS
+        </Typography>
+      </AppBar>
+      {/* VideoPlayer two camara */}
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+      {/* Options inside Notification */}
+    </div>
+  );
+};
 
 export default App;
